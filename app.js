31b678af6937e0543b186949356d11defe6868c7
Joel Dousset <jldc249@gmail.com>
2021-12-23 18:33:08 +0100

    navbar, css, image

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-
 // register view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
 
 // middleware & static files
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use('/img', express.static('img'));
 
-app.get("/", (req, res) => {
-  res.render("home", { title: "Home" });
+app.get('/', (req, res) => {
+  res.render('home', { title: 'Home' });
 });
 
-app.get("/expertise", (req, res) => {
-  res.render("expertise", { title: "Expertise" });
+app.get('/expertise', (req, res) => {
+  res.render('expertise', { title: 'Expertise' });
 });
 
-app.get("/honoraires", (req, res) => {
-  res.render("honoraires", { title: "Honoraires" });
+app.get('/honoraires', (req, res) => {
+  res.render('honoraires', { title: 'Honoraires' });
 });
 
-
-app.get("/contact", (req, res) => {
-  res.render("contact", { title: "Contact" });
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404", { title: "404" });
+  res.status(404).render('404', { title: '404' });
 });
---
+++ js/topButton.js
@@ -0,0 +1,24 @@
+// Get the button
+returnButton = document.querySelector('.returnTop');
+
+// If on top fade the bouton out, else fade it in
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    returnButton.style.display = 'block';
+  } else {
+    returnButton.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
